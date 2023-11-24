edd98cebf9756c3fa4e02aa31224733700095d2f
maxm <lynchnost@gmail.com>
2023-11-24 20:46:08 +0200

    Chapter 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { ReactNode } from 'react'
+
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+
+type PropTypes = {
+    children: ReactNode
+}
+
+export default function RootLayout({ children }: PropTypes) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
-const styleguide = require('@vercel/style-guide/prettier');
-
 module.exports = {
-  ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
-};
+  singleQuote: true,
+  printWidth: 90,
+  semi: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
